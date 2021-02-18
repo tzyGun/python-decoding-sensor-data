26593a0fd38151021ee6ecdbd793dcfff89e5349
Sylwester Denko <sylwek@Sylwesters-MacBook-Pro.local>
2021-02-18 21:59:30 +0100

    loading_data

---
+++ sensor/load_data.py
@@ -0,0 +1,10 @@
+import os
+import glob
+import csv
+
+def load_sensor_data():
+    sensor_data = []
+    sensor_files = glob.glob(os.path.join(os.getcwd(),'datasets', '*.csv'))
+    for sensor_file in sensor_files:
+        with open(sensor_file) as data_file:
+            data_reader = csv.DictReader(data_file)
