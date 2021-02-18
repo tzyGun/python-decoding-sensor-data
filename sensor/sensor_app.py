c0037f95bc4dcae21c2f3594510d3c60ce46b924
Sylwester Denko <sylwek@Sylwesters-MacBook-Pro.local>
2021-02-18 22:42:15 +0100

    load_data2

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,5 @@
 # Runner script for all modules
-from sensor import load_data
+from load_data import load_sensor_data
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -8,7 +8,7 @@ print("Sensor Data App")
 ##############################
 
 # Module 1 code here:
-data = load_data.load_sensor_data()
+data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 
